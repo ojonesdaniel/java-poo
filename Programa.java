66c317da1c8068ba0e1f9a0efb6796639ea01be5
Marco Túlio Salvador Filho <mtsf.salvador@gmail.com>
2024-03-22 20:20:00 -0300

    feat: o mundo é dos espertos

--- Programa.java
+++ Programa.java
@@ -1,6 +1,7 @@
 public class Programa {
     public static void main(String[] args) {
         System.out.println("Oi");
-        System.out.println("Lucas esteve aqui ");
+        System.out.println("Lucas esteve aqui");
+        System.out.println("Marco esteve aqui. Se leu mamou!");
     }
 }
